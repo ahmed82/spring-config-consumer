b4f2eb1985cb813bfab145cb1a8afbd56e86b715
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-01-16 10:12:37 -0500

    Spring Cloud Config Client init project

---
+++ src/test/java/com/atr/rent/configconsumer/ConfigConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atr.rent.configconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
